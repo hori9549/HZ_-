620168635ec28b0d142f5378a802878cbc9b6c34
yoshiyuki <hori9549@gmail.com>
2021-07-10 14:24:46 +0900

    shown

--- frmM社員マスタ.Designer.vb
+++ frmM社員マスタ.Designer.vb
@@ -1,7 +1,7 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class frmM社員マスタ
-    Inherits System.Windows.Forms.Form
-    ' Inherits frmベース
+    'Inherits System.Windows.Forms.Form
+    Inherits frmベース
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
     <System.Diagnostics.DebuggerNonUserCode()> _
