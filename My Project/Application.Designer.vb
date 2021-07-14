f275029da85e0a758c7efb28134657b3f28733b6
yoshiyuki <hori9549@gmail.com>
2021-07-15 08:10:35 +0900

    test

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.IMECSystem.Form1
+            Me.MainForm = Global.IMECSystem.frmメニュー
         End Sub
     End Class
 End Namespace
